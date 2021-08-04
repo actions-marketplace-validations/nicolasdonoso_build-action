769736472df4acb2907463e83a1831025d33c14a
Nico ʕ•ᴥ•ʔ <niko@swishanalytics.com>
2021-08-04 15:17:49 -0700

    more env var changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
 export PROJECT_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
+export TAG=$GITHUB_JOB
 echo "Building docker image $PROJECT_NAME"
 
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
-docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_JOB .
-docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_JOB
+docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG .
+docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
