a39d3785a0f341837445e8c38eef1164b1cd8c73
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 15:44:34 -0800

    logic changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,17 @@ else
 fi
 
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
-if [[ $GITHUB_REF_NAME == 'master' ]];
-    then 
-    export TAG="latest"
-    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
-    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
-    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
-else
-    export TAG=$GITHUB_RUN_ID
-    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
-    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
-fi
+for i in $(echo $image|tr ',' '\n')
+do 
+    if [[ $GITHUB_REF_NAME == 'master' ]];
+        then 
+        export TAG="latest"
+        docker build -f $i -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
+        docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
+        docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
+    else
+        export TAG=$GITHUB_RUN_ID
+        docker build -f $i -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
+        docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
+    fi
+done
