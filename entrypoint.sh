07d94237ed01bd6d9bd50af28a5e8f9191349fe2
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 11:20:43 -0800

    remove tag definition and put value directly in command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,10 @@ echo $GITHUB_REF_NAME
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
 if [[ $GITHUB_REF_NAME == 'master' ]];
     then 
-    export TAG="latest"
-    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_RUN_ID .
-    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
+    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:latest -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_RUN_ID .
+    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:latest
     docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_RUN_ID
 else
-    export TAG=$GITHUB_RUN_ID
     docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG .
     docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
 fi
