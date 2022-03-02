e99225866b279d3a5a57250be17a00d9b9e7950c
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 11:19:53 -0800

    add diff build logics depending on branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -l
 
 export PROJECT_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
-export TAG=$GITHUB_RUN_ID
 echo "Building docker image $PROJECT_NAME"
+echo $GITHUB_REF_NAME
 
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
-docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG .
-docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
+if [[ $GITHUB_REF_NAME == 'master' ]];
+    then 
+    export TAG="latest"
+    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_RUN_ID .
+    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
+    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$GITHUB_RUN_ID
+else
+    export TAG=$GITHUB_RUN_ID
+    docker build -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG .
+    docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME:$TAG
+fi
