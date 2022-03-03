e8056851ada35e352cb2b4c2930d8df26c5bc6c8
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 16:46:53 -0800

    logic changes for repo definition

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/sh -l
 
 echo "Building docker image $REPO_NAME"
-if [[ -z $ECR_REPO ]]
-    then export REPO_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
-else
-    export REPO_NAME=$ECR_REPO
-fi
 
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
 for image in $(echo $images|tr ',' '\n')
-do
-    echo "building $image"
+do  
+    export suffix=$(echo $image|cut -d '.' -f2)
+    if [[ -z $ECR_REPO ]]
+        then echo "ECR repo name defined by repo name"
+        export REPO_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
+    elif [[ $suffix != "" ]]
+        then echo "ECR repo name defined by image suffix"
+        export REPO_NAME=$suffix
+    else
+        echo "ECR repo name defined by env var ECR_REPO"
+        export REPO_NAME=$ECR_REPO
+    fi
+    echo "building image: $image, repo: $ECR_REPO, tag: $TAG"
     if [[ $GITHUB_REF_NAME == 'master' ]];
         then 
         export TAG="latest"
@@ -18,7 +24,7 @@ do
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
     else
-        export TAG=$GITHUB_RUN_ID
+        export TAG=$GITHUB_RUN_IDcccccbcdlig
         docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
     fi
