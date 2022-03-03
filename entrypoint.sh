89e4ff3b8babc6733d2ae3c76b8183ed0b464e07
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 16:04:52 -0800

    fix for loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,18 @@ else
 fi
 
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
-for i in $(echo $image|tr ',' '\n')
-do 
+for image in $(echo $images|tr ',' '\n')
+do
+    echo "building $image"
     if [[ $GITHUB_REF_NAME == 'master' ]];
         then 
         export TAG="latest"
-        docker build -f $i -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
+        docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
     else
         export TAG=$GITHUB_RUN_ID
-        docker build -f $i -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
+        docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
     fi
 done
