83f807b797e47c6265f57dfbe35234db6d11ca10
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 18:01:20 -0800

    tag def removed from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,10 @@ if [[ -z $DOCKERFILE_LOCATION ]]
         echo "building image: $image, repo: $ECR_REPO"
         if [[ $GITHUB_REF_NAME == 'master' ]];
             then 
-            export TAG="latest"
             docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
             docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
             docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
         else
-            export TAG=$GITHUB_RUN_ID
             docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
             docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
         fi
