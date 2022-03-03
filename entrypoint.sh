9680910a21b33fb9d460b6c5202dcf81ec3f90f2
Nicolas Donoso <nicolasdonoso@Nicolass-MacBook-Pro.local>
2022-03-02 17:41:18 -0800

    entrypoint changes to handle diff build logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,43 @@
 
 echo "Building docker image $REPO_NAME"
 
+if [[ -z $ECR_REPO ]]
+    then echo "ECR repo name defined by repo name"
+    export REPO_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
+else
+    echo "ECR repo name defined by env var ECR_REPO"
+    export REPO_NAME=$ECR_REPO
+fi
 aws ecr get-login --region $AWS_REGION --no-include-email | sh;
-for image in $(echo $images|tr ',' '\n')
-do  
-    export suffix=$(echo $image|cut -d '.' -f2)
-    if [[ -z $ECR_REPO ]]
-        then echo "ECR repo name defined by repo name"
-        export REPO_NAME=$(echo $GITHUB_REPOSITORY|cut -d '/' -f2)
-    elif [[ $suffix != "Dockerfile" ]]
-        then echo "ECR repo name defined by image suffix"
-        export REPO_NAME=$suffix
-    else
-        echo "ECR repo name defined by env var ECR_REPO"
-        export REPO_NAME=$ECR_REPO
-    fi
-    echo "building image: $image, repo: $ECR_REPO"
-    if [[ $GITHUB_REF_NAME == 'master' ]];
+if [[ -z $DOCKERFILE_LOCATION ]]
+    for image in $(echo $images|tr ',' '\n')
+    do  
+        export suffix=$(echo $image|cut -d '.' -f2)
+        if [[ $suffix != "Dockerfile" ]]
+            then echo "ECR repo name defined by image suffix"
+            export REPO_NAME=$suffix
+        fi
+        echo "building image: $image, repo: $ECR_REPO"
+        if [[ $GITHUB_REF_NAME == 'master' ]];
+            then 
+            export TAG="latest"
+            docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
+            docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
+            docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
+        else
+            export TAG=$GITHUB_RUN_ID
+            docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
+            docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
+        fi
+    done
+else
+    if [[ $GITHUB_REF_NAME == 'master' ]] || [[ $GITHUB_REF_NAME == 'main' ]];
         then 
-        export TAG="latest"
-        docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
+        docker build -f $DOCKERFILE_LOCATION -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID .
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$GITHUB_RUN_ID
     else
-        export TAG=$GITHUB_RUN_ID
-        docker build -f $image -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
+        docker build -f $DOCKERFILE_LOCATION -t $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG .
         docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$TAG
     fi
-done
+fi
